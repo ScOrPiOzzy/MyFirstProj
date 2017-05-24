d4b212a1af03ee424127a1754a8df4e7f86dedf4
张振宇 <sco_pra@126.com>
2017-05-24 22:55:13 +0800

    add a case

--- src/test/java/test/TestCase.java
+++ src/test/java/test/TestCase.java
@@ -1,7 +1,14 @@
 package test;
 
+import org.junit.Test;
+
 public class TestCase {
 	public static void main(String[] args) {
 		System.out.println("TestCase.main()");
+	}
+	
+	@Test
+	public void testMethod() throws Exception {
+		
 	}
 }
